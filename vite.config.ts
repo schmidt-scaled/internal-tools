193cd6b499a61a1666e6dd517d51e813e755f7b6
michael <michael.schmidt1379@gmail.com>
2025-08-26 20:32:21 +0300

    fix and a few fields added

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
